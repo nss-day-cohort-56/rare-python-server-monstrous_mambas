24dc9b6d972aeded7064769d4f521e16114fcec4
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-07-23 07:21:14 -0500

    Post new comment function added

--- request_handler.py
+++ request_handler.py
@@ -4,7 +4,7 @@ from views.tag_requests import create_tag, get_all_tags
 from views.post_request import delete_post, edit_post, get_all_post, get_single_post, create_new_post, get_posts_by_user_id
 from views import get_all_categories, get_single_category, get_all_users, create_category, delete_category
 from views.user_requests import create_user, login_user, get_single_user
-from views.comment_requests import get_all_comments_by_id
+from views.comment_requests import create_new_comment, get_all_comments_by_id
 
 
 class HandleRequests(BaseHTTPRequestHandler):
@@ -114,6 +114,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             response = create_tag(post_body)
         if resource == "posts":
             response = create_new_post(post_body)
+        if resource == "comments":
+            response = create_new_comment(post_body)
 
         self.wfile.write(response.encode())
 
